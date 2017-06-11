79cb5b8678a7e7d41171205f372481b41cc17fd3
Ben Sarrazin <ben@7swift.io>
2017-06-11 16:02:08 -0400

    QuickSpec template for Xcode

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+ln -s $PWD/quickspec/ ~/Library/Developer/Xcode/Templates/quickspec-template
+echo "Install complete. Please restart XCode (if applicable)."
---
+++ quickspec/QuickSpec.xctemplate/___FILEBASENAME___Spec.swift
@@ -0,0 +1,14 @@
+import Nimble
+import Quick
+
+class ___FILEBASENAMEASIDENTIFIER___Spec: QuickSpec {
+    
+    override func spec() {
+        describe("example") {
+            it("equals Hello, World!") {
+                expect("Hello, World!").to(equal("Hello, World!"))
+            }
+        }
+    }
+    
+}
